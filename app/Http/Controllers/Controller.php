ee36cfd0bd143ab62879f4299ca324aef484ca58
Pedro Lucas <pedro.lucaswpl@gmail.com>
2023-08-10 18:57:16 -0300

    feat: adiciona paginação nos resultados da API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DomainException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,26 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function paginateResults(int $perPage, int $page, array $data): array
+    {
+        $totalItems = count($data);
+        $startIndex = ($page - 1) * $perPage;
+
+        if ($startIndex >= $totalItems) {
+            $message = "A página {$page} é inválida. Total de itens: {$totalItems}, Itens por página: {$perPage}";
+            throw new DomainException($message, 400);
+        }
+
+        $endIndex = min($startIndex + $perPage - 1, $totalItems - 1);
+        $paginatedData = array_slice($data, $startIndex, $endIndex - $startIndex + 1);
+
+        return [
+            'data' => $paginatedData,
+            'current_page' => $page,
+            'per_page' => $perPage,
+            'total' => $totalItems,
+            'last_page' => ceil($totalItems / $perPage),
+        ];
+    }
 }
--- app/Http/Controllers/UfController.php
+++ app/Http/Controllers/UfController.php
@@ -2,19 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use DomainException;
 use App\Http\Constants\Ufs;
 use App\Http\Services\CitiesService;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class UfController extends Controller
 {
-    public function listCities(string $uf, CitiesService $citiesService)
+    public function listCities(string $uf, CitiesService $citiesService, Request $request): JsonResponse
     {
+        $perPage = $request->input('per_page', 100);
+        $page = $request->input('page', 1);
+
         if (!in_array(strtoupper($uf), Ufs::POSSIBLES)) {
-            throw new HttpException(400, 'UF inválida');
+            throw new DomainException('UF inválida', 400);
         }
         $cities = $citiesService->listCitiesOfUf($uf);
 
-        return response()->json($cities);
+        return response()->json($this->paginateResults($perPage, $page, $cities));
     }
 }
