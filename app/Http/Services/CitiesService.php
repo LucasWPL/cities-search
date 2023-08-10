8307a80e0bd849ee4344fd5ec56493aa721a81a0
Pedro Lucas <pedro.lucaswpl@gmail.com>
2023-08-10 18:12:28 -0300

    feat: adiciona cache às consultas para a API

--- app/Http/Services/CitiesService.php
+++ app/Http/Services/CitiesService.php
@@ -2,16 +2,41 @@
 
 namespace App\Http\Services;
 
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class CitiesService
 {
-    public function listCitiesOfUf(string $uf)
+    public function listCitiesOfUf(string $uf): array
     {
-        $url = $this->getServiceUrl($uf);
-        $response = Http::get($url);
+        $cacheKey = 'cities_' . strtoupper($uf);
+        if (Cache::has($cacheKey)) {
+            Log::info('Recuperando dados do cache', ['key', $cacheKey]);
 
-        return json_decode($response->body(), true);
+            return Cache::get($cacheKey);
+        }
+
+        try {
+            $response = Http::get($this->getServiceUrl($uf));
+            $response->throw();
+
+            $cities = json_decode($response->body(), true);
+            $this->saveOnCache($cities, $cacheKey);
+
+            return $cities;
+        } catch (RequestException $exception) {
+            Log::error('Erro na chamada à API', ['error_message' => $exception->getMessage()]);
+
+            throw new \DomainException('Ocorreu um erro na chamada à API', 500);
+        }
+    }
+
+    private function saveOnCache(array $data, string $key): void
+    {
+        Log::info('Inserindo dados no cache', ['key', $key]);
+        Cache::put($key, $data, 60 * 60 * 24);
     }
 
     private function getServiceUrl(string $uf): string
